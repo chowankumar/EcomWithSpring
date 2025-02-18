faca142a30476a70c4a507b9a6980535b11c3de3
chowan kumar <chowanmalhani5@gmail.com>
2025-02-18 11:54:26 +0500

    set the database

--- src/main/java/com/product/productcatalog/ProductcatalogApplication.java
+++ src/main/java/com/product/productcatalog/ProductcatalogApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductcatalogApplication {
+public class  ProductcatalogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductcatalogApplication.class, args);
